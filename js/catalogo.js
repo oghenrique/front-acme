c4af4430728b565064a7a64e723bfcf396e59ffc
oghenrique <santosguhenrique@gmail.com>
2024-03-25 10:50:36 -0300

    integrando com back

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,16 +1,35 @@
 'use strict'
 
 document.addEventListener("DOMContentLoaded", function () {
-    const card = document.querySelector(".card")
+  const card = document.querySelector(".card")
 
-    card.addEventListener("click", function () {
-      const cardInner = this.querySelector(".card-inner")
-      cardInner.style.transform = cardInner.style.transform === 'rotateY(180deg)' ? 'rotateY(0deg)' : 'rotateY(180deg)'
-    })
-
-    const modalBotao = document.querySelector('.mais')
-    modalBotao.addEventListener('click', function () {
-      var myModal = new bootstrap.Modal(document.getElementById('modalExemplo'))
-      myModal.show()
-    })
+  card.addEventListener("click", function () {
+    const cardInner = this.querySelector(".card-inner")
+    cardInner.style.transform = cardInner.style.transform === 'rotateY(180deg)' ? 'rotateY(0deg)' : 'rotateY(180deg)'
   })
+
+  const modalCloseButton = document.querySelector('.modal-header .btn-close')
+  modalCloseButton.addEventListener('click', function () {
+    const modalBackdrop = document.querySelector('.modal-backdrop')
+    modalBackdrop.remove()
+  })
+
+  const modalBotao = document.querySelector('.mais')
+  modalBotao.addEventListener('click', function () {
+    var myModal = new bootstrap.Modal(document.getElementById('modalExemplo'))
+    myModal.show()
+  })
+})
+
+function searchMovies(value) {
+  var cards = document.querySelectorAll('.card')
+  cards.forEach(card => {
+    var title = card.querySelector('.titulo-card').innerText.toLowerCase()
+    if (title.includes(value.toLowerCase())) {
+      card.classList.remove('hidden')
+    } else {
+      card.classList.add('hidden')
+    }
+  })
+}
+
---
+++ js/main.js
@@ -0,0 +1,46 @@
+import { getFilmes, getFilme, postFilme } from "./filmes.js"
+
+function criarCard(filme) {
+    
+    const card = document.createElement('div')
+    const titulo = document.createElement('h2')
+    titulo.textContent = filme.nome
+
+    const valor = document.createElement('button')
+    valor.textContent = filme.valor_unitario
+        
+    card.append(titulo, valor)
+
+    return card
+}
+
+async function preencherContainer(){
+    
+    const container = document.querySelector('body')
+    const filmes = await getFilmes()
+
+    filmes.forEach(filme => {
+        const card = criarCard(filme)
+        container.appendChild(card)
+    })
+    
+}
+
+preencherContainer()
+
+const filme =  {
+        "nome": "Avengers",
+        "sinopse": "Loki, o irmão de Thor, ganha acesso ao poder ilimitado do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D. Nick Fury, o diretor desta agência internacional que mantém a paz, logo reúne os únicos super-heróis que serão capazes de defender a Terra de ameaças sem precedentes. Homem de Ferro, Capitão América, Hulk, Thor, Viúva Negra e Gavião Arqueiro formam o time dos sonhos de Fury, mas eles precisam aprender a colocar os egos de lado e agir como um grupo em prol da humanidade.",
+        "duracao": "02:35:00",
+        "data_lancamento": "2022-08-25",
+        "data_relancamento": null,
+        "foto_capa": "https://exemplo.com/foto_vale.jpg",
+        "valor_unitario": 29.98
+      }
+
+postFilme(filme)
+
+
+
+
+console.log(postFilme(filme))
