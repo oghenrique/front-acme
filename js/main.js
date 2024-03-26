b9e177c76b71fd1ce03c5dc297d7558b9fe1dd28
oghenrique <santosguhenrique@gmail.com>
2024-03-25 22:54:12 -0300

    get com erro

--- js/main.js
+++ js/main.js
@@ -1,46 +1,56 @@
-import { getFilmes, getFilme, postFilme } from "./filmes.js"
+import { getFilmes, postFilme } from "./filmes.js"
 
-function criarCard(filme) {
-    
-    const card = document.createElement('div')
-    const titulo = document.createElement('h2')
+async function criarCard(filme) {
+    const container = document.createElement('div')
+    container.classList.add('card')
+
+    const cardInner = document.createElement('div')
+    cardInner.classList.add('card-inner')
+
+    const cardFront = document.createElement('div')
+    cardFront.classList.add('card-front')
+
+    const imagem = document.createElement('img')
+    imagem.src = filme.foto_capa
+    imagem.alt = `Poster do filme ${filme.nome}`
+    imagem.style.width = "100%"
+    imagem.style.height = "100%"
+    imagem.style.borderRadius = "16px"
+
+    cardFront.appendChild(imagem)
+
+    const cardBack = document.createElement('div')
+    cardBack.classList.add('card-back')
+
+    const titulo = document.createElement('h5')
+    titulo.classList.add('titulo-card')
     titulo.textContent = filme.nome
 
-    const valor = document.createElement('button')
-    valor.textContent = filme.valor_unitario
-        
-    card.append(titulo, valor)
+    const botaoMais = document.createElement('button')
+    botaoMais.classList.add('mais')
+    botaoMais.dataset.bsToggle = 'modal'
+    botaoMais.dataset.bsTarget = '#modalExemplo'
+    botaoMais.innerHTML = '<p>+</p>'
 
-    return card
+    const valor = document.createElement('h5')
+    valor.classList.add('valor-card')
+    valor.textContent = `R$${filme.valor_unitario.toFixed(2)}`
+
+    cardBack.append(titulo, botaoMais, valor)
+    cardInner.append(cardFront, cardBack)
+    container.appendChild(cardInner)
+
+    return container
 }
 
-async function preencherContainer(){
-    
-    const container = document.querySelector('body')
+async function preencherContainer() {
+    const container = document.querySelector('.card-container')
     const filmes = await getFilmes()
 
-    filmes.forEach(filme => {
-        const card = criarCard(filme)
+    filmes.forEach(async (filme) => {
+        const card = await criarCard(filme)
         container.appendChild(card)
     })
-    
 }
 
 preencherContainer()
-
-const filme =  {
-        "nome": "Avengers",
-        "sinopse": "Loki, o irmão de Thor, ganha acesso ao poder ilimitado do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D. Nick Fury, o diretor desta agência internacional que mantém a paz, logo reúne os únicos super-heróis que serão capazes de defender a Terra de ameaças sem precedentes. Homem de Ferro, Capitão América, Hulk, Thor, Viúva Negra e Gavião Arqueiro formam o time dos sonhos de Fury, mas eles precisam aprender a colocar os egos de lado e agir como um grupo em prol da humanidade.",
-        "duracao": "02:35:00",
-        "data_lancamento": "2022-08-25",
-        "data_relancamento": null,
-        "foto_capa": "https://exemplo.com/foto_vale.jpg",
-        "valor_unitario": 29.98
-      }
-
-postFilme(filme)
-
-
-
-
-console.log(postFilme(filme))
